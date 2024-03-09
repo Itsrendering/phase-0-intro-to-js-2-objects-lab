26ef3e030780e6ea4c6d86edc547a70837c912a6
Juanpaolo Delacruz <jpmd.creates@gmail.com>
2024-03-09 14:10:15 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St',
+}
+// employeeFunctions.js
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  };
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+  };
+  
+  const deleteFromEmployeeByKey = (employee, key) => {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  };
+  
+  const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+  };
+  
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
+  
